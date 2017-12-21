d44677d7bbc684c987ccdb1132480c947fbd6568
Willie Loh <willie@mindvalley.com>
2017-12-21 18:20:24 +0800

    optimized routing

--- lib/route.ex
+++ lib/route.ex
@@ -1,15 +1,25 @@
 defmodule Route do
-  def find_next(coordinate, map, traveled \\ {}) do
-    traveled = Tuple.append(traveled, coordinate)
+  def find_next(coordinate, map) do
+    find_next(routes(coordinate, map), {coordinate}, map)
+  end
+
+  defp find_next([], traveled, _map), do: traveled
+  defp find_next(routes, traveled, map) do
+    Enum.map(routes, fn(coor) ->
+      find_next(routes(coor, map), Tuple.append(traveled, coor), map)
+    end)
+  end
+
+  defp routes(coordinate, map) do
     current_value = Map.fetch!(map, coordinate)
 
-    Enum.map(directions(coordinate), fn(coor) ->
+    Enum.filter(directions(coordinate), fn(coor) ->
       with {:ok, next_value} <- Map.fetch(map, coor),
-           true <- current_value > next_value
+        true <- current_value > next_value
       do
-        find_next(coor, map, traveled)
+        true
       else
-        _ -> traveled
+        _ -> false
       end
     end)
   end
